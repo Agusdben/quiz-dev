57724d2c5c09f7f5728485dea9f192433845c211
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-05-17 19:40:09 -0300

    added Head next component

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,4 +1,5 @@
 import { Rubik } from 'next/font/google'
+import Head from 'next/head'
 
 interface Props {
   children: React.ReactNode
@@ -8,13 +9,21 @@ const rubik = Rubik({ subsets: ['latin'], weight: '500' })
 
 const Layout: React.FC<Props> = ({ children }) => {
   return (
-    <div className={`${rubik.className} w-screen h-screen overflow-x-hidden overflow-y-auto flex flex-col`}>
-      <header>header</header>
-      <main className='flex-1'>
-        {children}
-      </main>
-      <footer>footer</footer>
-    </div>
+    <>
+      <Head>
+        <title>DevQuiz</title>
+        <meta name="description" content="Site where you can solve quizzes about dev world and test your knowledge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={`${rubik.className} w-screen max-w-5xl m-auto min-h-screen overflow-x-hidden flex flex-col gap-4`}>
+        <header>header</header>
+        <main className='flex-1'>
+          {children}
+        </main>
+        <footer>footer</footer>
+      </div>
+    </>
   )
 }
 
