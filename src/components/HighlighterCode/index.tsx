a3968c195f85ed5a1171e0e5efe95756767fc0f1
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-05-20 16:31:35 -0300

    modified HighlighterCode component

--- src/components/HighlighterCode/index.tsx
+++ src/components/HighlighterCode/index.tsx
@@ -1,14 +1,13 @@
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
-import { tomorrow } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 interface Props {
-  language: 'javascript'
+  language: string
   code: string
 }
 
 const HighlighterCode: React.FC<Props> = ({ language, code }) => {
   return (
-    <SyntaxHighlighter language={language} style={tomorrow}>
+    <SyntaxHighlighter language={language}>
       {code}
     </SyntaxHighlighter>
   )
