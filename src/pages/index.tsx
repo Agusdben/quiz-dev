fbb18c1f10acabe2d75a3492f9510b0adbcce736
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-05-20 16:33:42 -0300

    prototype home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,11 +8,17 @@ interface Props {
 
 const Home: React.FC<Props> = ({ quizzes }) => {
   return (
-    <section>
-      <ul className=''>
+    <section className='w-full h-full flex flex-col gap-4'>
+      <header className='text-center flex  justify-between'>
+        <h3>List of quizzes</h3>
+        <form>
+          <input placeholder='Search'></input>
+        </form>
+      </header>
+      <ul className='grid grid-cols-auto-fill-md gap-4 gap-y-6'>
         {quizzes.map(q => {
           return (
-            <li key={q.id} className='max-w-xs grid border-rose-50 border-2 border-dashed p-2 text-center hover:border-blue-400'>
+            <li key={q.id} className='max-w-xs w-full grid border-rose-50 border-2 border-dashed p-2 text-center hover:border-blue-400'>
               <Link href={`quizzes/${q.id}`} className='flex flex-col hover:bg-slate-100 hover:text-black'>
                 <h3 className='text-blue-400'>{q.title}</h3>
                 <span>{q.language}</span>
