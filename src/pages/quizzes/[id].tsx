c44f77f3ce3603be82bf9b62342a050600a67acb
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-05-20 16:35:15 -0300

    minified code and separated in components

--- src/pages/quizzes/[id].tsx
+++ src/pages/quizzes/[id].tsx
@@ -1,21 +1,19 @@
+import AnswersHistory from '@/components/AnswersHistory'
 import HighlighterCode from '@/components/HighlighterCode'
+import InteractiveQuestion from '@/components/InteractiveQuestion'
 import useSolveQuiz from '@/hooks/useSolveQuiz'
 import { type AnswerIndexes, type ServerSideError } from '@/types'
 import { type Question } from '@/types/question'
 import { type Quiz } from '@/types/quiz'
 import { type GetServerSideProps } from 'next'
 import NextError from 'next/error'
+import Link from 'next/link'
 import { useState } from 'react'
 
 interface Props {
   quiz: Quiz
   questions: Question[]
   error?: ServerSideError
-}
-
-const ATTEMPT_STYLES = {
-  0: 'bg-red-300 text-black',
-  1: 'bg-green-300 text-black'
 }
 
 const QuizPage: React.FC<Props> = ({ error, quiz, questions }) => {
@@ -29,6 +27,7 @@ const QuizPage: React.FC<Props> = ({ error, quiz, questions }) => {
 
   const [isAttemptCorrect, setIsAttemptCorrect] = useState<0 | 1 | undefined>(undefined)
   const [attemptIndex, setAttemptIndex] = useState<AnswerIndexes | null>(null)
+  const [answersHistory, setAnswersHistory] = useState<Array<0 | 1 | undefined>>(new Array(totalQuestions).fill(undefined))
 
   if (error != null) {
     return (
@@ -38,7 +37,13 @@ const QuizPage: React.FC<Props> = ({ error, quiz, questions }) => {
 
   const handleAttempt = (answerIndex: AnswerIndexes): void => {
     const isCorrect = isAnswerCorrect({ answerIndex })
-    setIsAttemptCorrect(isCorrect ? 1 : 0)
+    setIsAttemptCorrect(isCorrect)
+    setAnswersHistory(history => {
+      const newHistory = [...history]
+      const undefinedIndex = newHistory.findIndex(h => h === undefined)
+      newHistory[undefinedIndex] = isCorrect
+      return newHistory
+    })
     setAttemptIndex(answerIndex)
 
     if (currentQuestionNumber !== totalQuestions) {
@@ -53,28 +58,20 @@ const QuizPage: React.FC<Props> = ({ error, quiz, questions }) => {
   return (
     <section className='flex-1 flex flex-col items-center justify-center'>
       <article className='max-w-lg flex flex-col gap-4 w-full p-2'>
-        <header>
-          <h2>Question: {currentQuestionNumber}/{totalQuestions}</h2>
+        <header className='flex flex-col gap-4'>
+          <h3 className='text-center uppercase text-2xl'>{quiz.title}</h3>
+          <AnswersHistory history={answersHistory} currentQuestionNumber={currentQuestionNumber}/>
         </header>
         <HighlighterCode code={currentQuestion.question} language={quiz.language} />
-        <div className='grid grid-cols-2 gap-4'>
-          {
-            currentQuestion.answers.map(answer => {
-              const attemptStyle = isAttemptCorrect !== undefined && attemptIndex === answer.index ? ATTEMPT_STYLES[isAttemptCorrect] : ''
-
-              return (
-                <button
-                 disabled={isAttemptCorrect !== undefined}
-                 key={answer.id}
-                 className={`text-left border-2 p-1 hover:bg-blue-400 disabled:pointer-events-none ${attemptStyle}`}
-                 onClick={() => { handleAttempt(answer.index) }}
-                 >
-                  <p>{answer.index}) {answer.answer}</p>
-                </button>
-              )
-            })
-          }
-        </div>
+        <InteractiveQuestion
+          isAttemptCorrect={isAttemptCorrect}
+          handleResponse={handleAttempt}
+          question={currentQuestion}
+          attemptIndex={attemptIndex}
+        />
+        <footer className='mt-6'>
+          <Link href='/' className='p-2 bg-red-300 text-black hover:bg-red-400 transition-colors'> &lt;- Exit</Link>
+        </footer>
       </article>
     </section>
   )
