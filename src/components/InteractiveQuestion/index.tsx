18c79b1575e9f11dd06996869e466574a04af7ba
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-05-24 20:01:42 -0300

    added functionality

--- src/components/InteractiveQuestion/index.tsx
+++ src/components/InteractiveQuestion/index.tsx
@@ -1,34 +1,54 @@
-import { ATTEMPT_STYLES } from '@/constants'
 import { type AnswerIndexes } from '@/types'
-import { type Question } from '@/types/question'
-import React from 'react'
+import { type Answer } from '@/types/answer'
+import { type QuestionId, type Question } from '@/types/question'
+import React, { useState } from 'react'
+import HighlighterCode from '../HighlighterCode'
 
 interface Props {
   question: Question
-  handleResponse: (answerIndex: AnswerIndexes) => void
-  isAttemptCorrect: 0 | 1 | undefined
-  attemptIndex: AnswerIndexes | null
+  handleResponse: ({ questionId, answerIndex }: {
+    questionId: QuestionId
+    answerIndex: AnswerIndexes
+  }) => Promise<void>
+  isAttemptCorrect: boolean | undefined
 }
 
-const InteractiveQuestion: React.FC<Props> = ({ question, handleResponse, isAttemptCorrect, attemptIndex }) => {
-  return (
-    <div className='grid md:grid-cols-2 gap-4'>
-      {
-        question.answers.map(answer => {
-          const attemptStyle = isAttemptCorrect !== undefined && attemptIndex === answer.index ? ATTEMPT_STYLES[isAttemptCorrect] : ''
+const ATTEMPT_STYLES = {
+  0: 'bg-red-300 text-black border-red-300',
+  1: 'bg-green-300 text-black border-green-300'
+}
 
-          return (
-            <button
-              disabled={isAttemptCorrect !== undefined}
-              key={answer.id}
-              className={`text-left border-2 p-2 hover:bg-blue-400 disabled:pointer-events-none transition-colors ${attemptStyle}`}
-              onClick={() => { handleResponse(answer.index) }}
-              >
-              <p>{answer.index}) {answer.answer}</p>
-            </button>
-          )
-        })
-      }
+const InteractiveQuestion: React.FC<Props> = ({ question, handleResponse, isAttemptCorrect }) => {
+  const [lastAttemptIndex, setLastAttemptIndex] = useState<AnswerIndexes | undefined>(undefined)
+
+  const handleAttempt = (answer: Answer): void => {
+    setLastAttemptIndex(answer.index)
+    handleResponse({ answerIndex: answer.index, questionId: question.id })
+      .catch(error => { console.error(error) })
+  }
+
+  return (
+    <div className='w-full flex flex-col gap-4'>
+      <HighlighterCode code={question.question} language='javascript'/>
+      <div className='grid md:grid-cols-2 gap-4'>
+        {
+          question.answers.map(answer => {
+            const attemptStyles = isAttemptCorrect !== undefined && lastAttemptIndex === answer.index ? ATTEMPT_STYLES[isAttemptCorrect ? 1 : 0] : ''
+            return (
+              <button
+                disabled={isAttemptCorrect !== undefined}
+                key={answer.id}
+                className={`text-left border-2 p-2 hover:bg-blue-400 disabled:pointer-events-none transition-colors
+                ${lastAttemptIndex === answer.index ? attemptStyles : ''}
+                `}
+                onClick={() => { handleAttempt(answer) }}
+                >
+                <p>{answer.index}) {answer.answer}</p>
+              </button>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }
