2f470d024ca706821cd2f52f8ec435871f8f7e6d
Agustin Di Benedetto <agustindibenedetto@hotmail.com>
2023-05-24 20:02:19 -0300

    added logo component

---
+++ src/components/Logo/index.tsx
@@ -0,0 +1,11 @@
+const Logo: React.FC = () => {
+  return (
+    <>
+      <div className="text-5xl w-full max-w-sm text-center text-black p-2 relative bg-black/50 py-12">
+        <p className="[&_span]:text-primary text-text"><span>&#123;</span> DevQuiz <span>&#125;</span></p>
+      </div>
+    </>
+  )
+}
+
+export default Logo
